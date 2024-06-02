2a42b0c9caaef6e7eb615fd990497747194388f5
Kiet <kietly019@gmail.com>
2024-06-02 18:37:06 +0700

    update

--- src/app/(admin)/admin/cv/page.tsx
+++ src/app/(admin)/admin/cv/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import React from 'react';
 
@@ -57,111 +57,113 @@ import { ThemeMode } from 'types/config';
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const { borderRadius } = useConfig();
+  const downMD = useMediaQuery(theme.breakpoints.down('md'));
 
-    const theme = useTheme();
-    const { borderRadius } = useConfig();
-    const downMD = useMediaQuery(theme.breakpoints.down('md'));
+  const [anchorEl, setAnchorEl] = React.useState<Element | null>(null);
+  const handleClick = (event: React.SyntheticEvent) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const [anchorEl, setAnchorEl] = React.useState<Element | null>(null);
-    const handleClick = (event: React.SyntheticEvent) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const [anchorSharedEl, setAnchorSharedEl] = React.useState<Element | null>(null);
 
-    const [anchorSharedEl, setAnchorSharedEl] = React.useState<Element | null>(null);
+  const handleSharedClick = (event: React.MouseEvent) => {
+    setAnchorSharedEl(event.currentTarget);
+  };
 
-    const handleSharedClick = (event: React.MouseEvent) => {
-        setAnchorSharedEl(event.currentTarget);
-    };
+  const handleSharedClose = () => {
+    setAnchorSharedEl(null);
+  };
 
-    const handleSharedClose = () => {
-        setAnchorSharedEl(null);
-    };
+  let commentsResult:
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>[] = <></>;
 
-    let commentsResult:
-        | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-        | React.ReactElement<any, string | React.JSXElementConstructor<any>>[] = <></>;
+  return (
+    <Grid container spacing={gridSpacing}>
+      <Grid item xs={12} sm={12} md={4} lg={6} style={{ margin: 'auto' }}>
+        <MainCard>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Grid container wrap="nowrap" alignItems="center" spacing={1}>
+                <Grid item>
+                  <Avatar alt="User 1" src={`/assets/images/users/avatar-1.png`} />
+                </Grid>
+                <Grid item xs zeroMinWidth>
+                  <Grid container alignItems="center" spacing={1}>
+                    <Grid item>
+                      <Typography variant="h5">Doan Ming Quang</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="caption">
+                        <FiberManualRecordIcon sx={{ width: '10px', height: '10px', opacity: 0.5, m: '0 5px' }} />
+                        12 June 2024
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <ButtonBase sx={{ borderRadius: '12px' }} onClick={handleClick} aria-label="Add to Cart product">
+                    <Avatar
+                      variant="rounded"
+                      sx={{
+                        ...theme.typography.commonAvatar,
+                        ...theme.typography.smallAvatar,
+                        bgcolor: theme.palette.mode === ThemeMode.DARK ? 'dark.main' : 'secondary.light',
+                        color: theme.palette.mode === ThemeMode.DARK ? 'dark.light' : 'secondary.dark',
+                        zIndex: 1,
+                        transition: 'all .2s ease-in-out',
+                        '&[aria-controls="menu-list-grow"],&:hover': { bgcolor: 'secondary.main', color: 'secondary.light' }
+                      }}
+                      aria-controls="menu-post"
+                      aria-haspopup="true"
+                    >
+                      <MoreVertTwoToneIcon fontSize="inherit" />
+                    </Avatar>
+                  </ButtonBase>
+                  <Menu
+                    id="menu-post"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                    variant="selectedMenu"
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'right'
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>Edit</MenuItem>
+                    <MenuItem onClick={handleClose}>Delete</MenuItem>
+                  </Menu>
+                </Grid>
+              </Grid>
+            </Grid>
 
-    return (
-        <Grid container spacing={gridSpacing}>
-            <Grid item xs={12} sm={12} md={4} lg={6} style={{ margin: 'auto' }}>
-                <MainCard>
-                    <Grid container spacing={1}>
-                        <Grid item xs={12}>
-                            <Grid container wrap="nowrap" alignItems="center" spacing={1}>
-                                <Grid item>
-                                    <Avatar alt="User 1" src={`/assets/images/users/avatar-1.png`} />
-                                </Grid>
-                                <Grid item xs zeroMinWidth>
-                                    <Grid container alignItems="center" spacing={1}>
-                                        <Grid item>
-                                            <Typography variant="h5">Doan Ming Quang</Typography>
-                                        </Grid>
-                                        <Grid item>
-                                            <Typography variant="caption">
-                                                <FiberManualRecordIcon sx={{ width: '10px', height: '10px', opacity: 0.5, m: '0 5px' }} />12 June 2024
-                                            </Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
-                                <Grid item>
-                                    <ButtonBase sx={{ borderRadius: '12px' }} onClick={handleClick} aria-label="Add to Cart product">
-                                        <Avatar
-                                            variant="rounded"
-                                            sx={{
-                                                ...theme.typography.commonAvatar,
-                                                ...theme.typography.smallAvatar,
-                                                bgcolor: theme.palette.mode === ThemeMode.DARK ? 'dark.main' : 'secondary.light',
-                                                color: theme.palette.mode === ThemeMode.DARK ? 'dark.light' : 'secondary.dark',
-                                                zIndex: 1,
-                                                transition: 'all .2s ease-in-out',
-                                                '&[aria-controls="menu-list-grow"],&:hover': { bgcolor: 'secondary.main', color: 'secondary.light' }
-                                            }}
-                                            aria-controls="menu-post"
-                                            aria-haspopup="true"
-                                        >
-                                            <MoreVertTwoToneIcon fontSize="inherit" />
-                                        </Avatar>
-                                    </ButtonBase>
-                                    <Menu
-                                        id="menu-post"
-                                        anchorEl={anchorEl}
-                                        keepMounted
-                                        open={Boolean(anchorEl)}
-                                        onClose={handleClose}
-                                        variant="selectedMenu"
-                                        anchorOrigin={{
-                                            vertical: 'bottom',
-                                            horizontal: 'right'
-                                        }}
-                                        transformOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'right'
-                                        }}
-                                    >
-                                        <MenuItem onClick={handleClose}>Edit</MenuItem>
-                                        <MenuItem onClick={handleClose}>Delete</MenuItem>
-                                    </Menu>
-                                </Grid>
-                            </Grid>
-                        </Grid>
+            {/* post - content */}
+            <Grid item xs={12} sx={{ '& > p': { ...theme.typography.body1, mb: 0 } }}>
+              <Markdown remarkPlugins={[remarkGfm]}>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                There are many variations of passages.
+              </Markdown>
+            </Grid>
 
-                        {/* post - content */}
-                        <Grid item xs={12} sx={{ '& > p': { ...theme.typography.body1, mb: 0 } }}>
-                            <Markdown remarkPlugins={[remarkGfm]}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. There are many variations of passages.</Markdown>
-                        </Grid>
+            {/* post - photo grid */}
+            <Grid item xs={12}>
+              <ImageList itemData={[{ img: 'cv.png', featured: true }]} />
+            </Grid>
 
-                        {/* post - photo grid */}
-                        <Grid item xs={12}>
-                            <ImageList itemData={[{ img: "cv.png", featured: true }]} />
-                        </Grid>
-
-
-                        {/* post - video */}
-                        {/* {data.video && (
+            {/* post - video */}
+            {/* {data.video && (
                             <Grid item xs={12} sx={{ '&.MuiGrid-root': { pt: 2 } }}>
                                 <CardMedia
                                     sx={{ borderRadius: `${borderRadius}px`, height: { xs: 220, lg: 330 } }}
@@ -171,86 +173,86 @@ const Dashboard = () => {
                             </Grid>
                         )} */}
 
-                        {/* post - comment, likes and replay history */}
-                        <Grid item xs={12}>
-                            <Grid
-                                container
-                                alignItems="center"
-                                justifyContent="space-between"
-                                spacing={2}
-                                sx={{ mt: 0, color: theme.palette.mode === ThemeMode.DARK ? 'grey.700' : 'grey.800' }}
-                            >
-                                <Grid item>
-                                    <Stack direction="row" spacing={2}>
-                                        <Button
-                                            variant="text"
-                                            onClick={() => { }}
-                                            color="inherit"
-                                            size="small"
-                                            startIcon={<ThumbUpAltTwoToneIcon color={true ? 'primary' : 'inherit'} />}
-                                        >
-                                            21
-                                            <Typography color="inherit" sx={{ fontWeight: 500, ml: 0.5, display: { xs: 'none', sm: 'block' } }}>
-                                                likes
-                                            </Typography>
-                                        </Button>
-                                        <Button
-                                            onClick={() => { }}
-                                            size="small"
-                                            variant="text"
-                                            color="inherit"
-                                            startIcon={<ChatBubbleTwoToneIcon color="secondary" />}
-                                        >
-                                            3 comments
-                                        </Button>
-                                    </Stack>
-                                </Grid>
-                                <Grid item>
-                                    <IconButton onClick={handleSharedClick} size="large" aria-label="more options">
-                                        <ShareTwoToneIcon sx={{ width: '16px', height: '16px' }} />
-                                    </IconButton>
-                                    <Menu
-                                        id="menu-post"
-                                        anchorEl={anchorSharedEl}
-                                        keepMounted
-                                        open={Boolean(anchorSharedEl)}
-                                        onClose={handleSharedClose}
-                                        variant="selectedMenu"
-                                        anchorOrigin={{
-                                            vertical: 'bottom',
-                                            horizontal: 'right'
-                                        }}
-                                        transformOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'right'
-                                        }}
-                                        sx={{
-                                            '& .MuiSvgIcon-root': {
-                                                marginRight: '14px',
-                                                fontSize: '1.25rem'
-                                            }
-                                        }}
-                                    >
-                                        <MenuItem onClick={handleSharedClose}>
-                                            <ShareTwoToneIcon fontSize="inherit" /> Share Now
-                                        </MenuItem>
-                                        <MenuItem onClick={handleSharedClose}>
-                                            <PeopleAltTwoToneIcon fontSize="inherit" /> Share to Friends
-                                        </MenuItem>
-                                        <MenuItem onClick={handleSharedClose}>
-                                            <ChatTwoToneIcon fontSize="inherit" /> Send in Messanger
-                                        </MenuItem>
-                                        <MenuItem onClick={handleSharedClose}>
-                                            <ContentCopyTwoToneIcon fontSize="inherit" /> Copy Link
-                                        </MenuItem>
-                                    </Menu>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </MainCard>
+            {/* post - comment, likes and replay history */}
+            <Grid item xs={12}>
+              <Grid
+                container
+                alignItems="center"
+                justifyContent="space-between"
+                spacing={2}
+                sx={{ mt: 0, color: theme.palette.mode === ThemeMode.DARK ? 'grey.700' : 'grey.800' }}
+              >
+                <Grid item>
+                  <Stack direction="row" spacing={2}>
+                    <Button
+                      variant="text"
+                      onClick={() => {}}
+                      color="inherit"
+                      size="small"
+                      startIcon={<ThumbUpAltTwoToneIcon color={true ? 'primary' : 'inherit'} />}
+                    >
+                      21
+                      <Typography color="inherit" sx={{ fontWeight: 500, ml: 0.5, display: { xs: 'none', sm: 'block' } }}>
+                        likes
+                      </Typography>
+                    </Button>
+                    <Button
+                      onClick={() => {}}
+                      size="small"
+                      variant="text"
+                      color="inherit"
+                      startIcon={<ChatBubbleTwoToneIcon color="secondary" />}
+                    >
+                      3 comments
+                    </Button>
+                  </Stack>
+                </Grid>
+                <Grid item>
+                  <IconButton onClick={handleSharedClick} size="large" aria-label="more options">
+                    <ShareTwoToneIcon sx={{ width: '16px', height: '16px' }} />
+                  </IconButton>
+                  <Menu
+                    id="menu-post"
+                    anchorEl={anchorSharedEl}
+                    keepMounted
+                    open={Boolean(anchorSharedEl)}
+                    onClose={handleSharedClose}
+                    variant="selectedMenu"
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'right'
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    sx={{
+                      '& .MuiSvgIcon-root': {
+                        marginRight: '14px',
+                        fontSize: '1.25rem'
+                      }
+                    }}
+                  >
+                    <MenuItem onClick={handleSharedClose}>
+                      <ShareTwoToneIcon fontSize="inherit" /> Share Now
+                    </MenuItem>
+                    <MenuItem onClick={handleSharedClose}>
+                      <PeopleAltTwoToneIcon fontSize="inherit" /> Share to Friends
+                    </MenuItem>
+                    <MenuItem onClick={handleSharedClose}>
+                      <ChatTwoToneIcon fontSize="inherit" /> Send in Messanger
+                    </MenuItem>
+                    <MenuItem onClick={handleSharedClose}>
+                      <ContentCopyTwoToneIcon fontSize="inherit" /> Copy Link
+                    </MenuItem>
+                  </Menu>
+                </Grid>
+              </Grid>
             </Grid>
-        </Grid>
-    );
+          </Grid>
+        </MainCard>
+      </Grid>
+    </Grid>
+  );
 };
 export default Dashboard;
