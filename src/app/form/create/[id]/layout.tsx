8a1c8c7a2d1fa485b86ef7ef90168432a8289fcd
Kiet <kietly019@gmail.com>
2024-06-10 10:17:52 +0700

    update

---
+++ src/app/form/create/[id]/layout.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useCheckRequestUrl } from 'hooks/use-check-request-url';
+import { redirect, useRouter } from 'next/navigation';
+import { ReactNode, useEffect } from 'react';
+import CircularLoader from 'ui-component/CircularLoader';
+
+export default function Layout({ params, children }: { params: { id: string }; children: ReactNode }) {
+  const [email, id] = params.id.split('-');
+  const { isLoading, isValid } = useCheckRequestUrl({ id });
+  const router = useRouter();
+  useEffect(() => {
+    if (!isLoading && !isValid) {
+      router.push('localhost:3000/not-found');
+    }
+  }, [isValid, isLoading]);
+  return (
+    <>
+      {isLoading ? <CircularLoader /> : null}
+      {!isLoading && isValid ? <>{children}</> : null}
+    </>
+  );
+
+}
