c02c987fb9f0609bc73875921501536b920c7bdd
Kiet <kiet019@gmail.com>
2024-09-24 19:43:37 +0700

    update

--- src/hooks/use-get-policy.ts
+++ src/hooks/use-get-policy.ts
@@ -1,69 +1,83 @@
-
 import { GetPolicy, Policy } from "package/api/policy";
-import { getPolicyById, GetPolicyByIdRq, PolicyById } from "package/api/policy/[id]";
+import {
+  getPolicyById,
+  GetPolicyByIdRq,
+  PolicyById,
+} from "package/api/policy/[id]";
 import { useEffect, useState } from "react";
 
-export const useGetPolicyById = (params: GetPolicyByIdRq, accessToken: string, refresh: number) => {
-    const [policyById, setPolicyById] = useState<PolicyById>({
-        id: 0,
-        key: '',
-        value: 0,
-        description: '',
-    });
+export const useGetPolicyById = (
+  params: GetPolicyByIdRq,
+  accessToken: string,
+  refresh: number
+) => {
+  const [policyById, setPolicyById] = useState<PolicyById>({
+    id: 0,
+    key: "",
+    value: 0,
+    description: "",
+  });
 
-    const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const fetchPolicyById = async () => {
-        if (!accessToken) {
-            return;
-        }
-        try {
-            setLoading(true);
-            const data = await getPolicyById(params, accessToken);
-            if (data.status !== "success") {
-                throw new Error(data.responseText);
-            }
-            setPolicyById(data.data);
-        } catch (error: any) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
+  const fetchPolicyById = async () => {
+    if (!accessToken) {
+      return;
     }
-
-    useEffect(() => { fetchPolicyById() }, [params.id, refresh, accessToken]);
-
-    return {
-        policyById, loading
+    try {
+      setLoading(true);
+      const data = await getPolicyById(params, accessToken);
+      if (data.status !== "success") {
+        throw new Error(data.responseText);
+      }
+      setPolicyById(data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-}
+  };
+
+  useEffect(() => {
+    fetchPolicyById();
+  }, [params.id, refresh, accessToken]);
+
+  return {
+    policyById,
+    loading,
+  };
+};
 
 export const useGetPolicy = (accessToken: string, refresh: number) => {
-    const [policy, setPolicy] = useState<Policy[]>([]);
+  const [policy, setPolicy] = useState<Policy[]>([]);
 
-    const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const fetchPolicy = async () => {
-        if (!accessToken) {
-            return;
-        }
-        try {
-            setLoading(true);
-            const data = await GetPolicy(accessToken);
-            if (data.status !== "success") {
-                throw new Error(data.responseText);
-            }
-            setPolicy(data.data);
-        } catch (error: any) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
+  const fetchPolicy = async () => {
+    if (!accessToken) {
+      return;
     }
-
-    useEffect(() => { fetchPolicy() }, [refresh, accessToken]);
-
-    return {
-        policy, loading
+    try {
+      setLoading(true);
+      const data = await GetPolicy(accessToken);
+      console.log(data)
+      if (data.status !== "success") {
+        throw new Error(data.responseText);
+      }
+      setPolicy(data.data);
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-}
+  };
+
+  useEffect(() => {
+    fetchPolicy();
+  }, [refresh, accessToken]);
+
+  return {
+    policy,
+    loading,
+  };
+};
